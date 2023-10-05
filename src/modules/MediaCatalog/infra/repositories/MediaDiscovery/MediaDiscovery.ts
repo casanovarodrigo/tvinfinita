733f6b72017dce669b0479d482a3fc80a4c1e7cc
casanovacsp <rodrigo.casanova@csptech.com.br>
2023-10-05 20:33:09 -0300

    feat: showing tvshows file names

--- src/modules/MediaCatalog/infra/repositories/MediaDiscovery/MediaDiscovery.ts
+++ src/modules/MediaCatalog/infra/repositories/MediaDiscovery/MediaDiscovery.ts
@@ -9,6 +9,9 @@ export class MediaDiscoveryClass implements IMediaDiscovery {
 	private storageFolderPath = ''
 	private availableTitlesFile = ''
 	private validatedFile = ''
+	private prohibitedFormats = new RegExp(/\.(srt)$/)
+	private allowedFormats = new RegExp(/\.(avi|mkv|mp4)$/)
+
 
 	constructor(storageFolderPath: string = 'storage',
 		availableTitlesFile: string = 'available-titles.json',
@@ -27,10 +30,6 @@ export class MediaDiscoveryClass implements IMediaDiscovery {
 			if (error)
 			fs.mkdirSync(this.storageFolderPath, { recursive: true })
 		}
-    // const validatedFolderExists = fs.mkdirSync(this.validatedFolderPath, { recursive: true })
-    // if (!validatedFolderExists){
-    //     fs.mkdirSync(this.validatedFolderPath, { recursive: true })
-    // }
 	}
 
 	/**
@@ -54,8 +53,8 @@ export class MediaDiscoveryClass implements IMediaDiscovery {
     const list = JSON.parse(unvalidatedFile)
 
 		const promises = Object.keys(list).map(async(titleKey) => {
-				let info = {}
-				if (list[titleKey].type = 'serie') info = await getSerieRegInfo(titleKey, list[titleKey])
+				let info: any = {}
+				if (list[titleKey].type = 'serie') info = await this.getSerieRegInfo(titleKey, list[titleKey])
 				// TO-DO: implement movie registration pipeline
 				// else if (title.type = 'filme') info = getMovieRegInfo(title)
 				// TO-DO: implement collection registration pipeline. Collections are small like trilogies or jackie chan movies
@@ -64,7 +63,88 @@ export class MediaDiscoveryClass implements IMediaDiscovery {
 				return info
 		})
 
-		console.log('list', list)
+		try {
+			console.log('list', list)
+
+			const titleList = await Promise.all(promises)
+
+			console.log('titleList', titleList)
+		} catch (error) {
+			console.log('registerTitles', error)
+		}
+
+	}
+
+	/**
+	 * Gets information about the show for registration
+	* in a linear way O(n)
+	* @param {String} titleKey title object key
+	* @param {Object} titleObj title info object
+	* @returns {Object} Title informations [name, titleKey, type, episodeCout, totalTitleDuration, seasonInfo]
+	* @resolve Array list of existing scenes
+	* @reject errorPipe handler
+	*/
+	public async getSerieRegInfo(titleKey: string, titleObj: any){
+
+		const seasonsList = await this.getTitleSubFiles(titleObj)
+    // merge all season files into one single array
+    const merged = [].concat.apply([], seasonsList);
+		const seasonsPromises = merged.map(async (episodeName) => {
+			const seasonNumberRegex = episodeName.match(/[s|S]([0-9]+)E[0-9]+/)
+			const seasonNumber = parseInt(seasonNumberRegex[1])
+			const path = titleObj.path + '/Temporada ' + seasonNumber
+
+			// const episodeMetadata = await getVideoMetadata(path + '/' + episodeName)
+			// console.log('episodeMetadata', episodeMetadata)
+			return {
+					fileName: episodeName,
+					// metadata: episodeMetadata
+			}			
+		})
+
+		try {
+			const episodeList = await Promise.all(seasonsPromises)
+			return episodeList
+		} catch (error) {
+			console.log('getSerieRegInfo error', error)
+		}
+
+		let seasonNumberRegex
+		let seasonNumber
+		let epNumberRegex
+		let epNumber
+		let tempInfo
+		let seasonsInfo = {}
+		let totalTitleDuration = 0
+		let totalEpisodeCount = 0
+		let doubleEpisode = false
+
+		
+	}
+
+	/**
+	 * Get title files from subfolders filtering unallowed file formats
+	 * @param {Object} titleObj title info of the title be found and returned
+	 * @returns {Array} bi-dimensional array of subfolders and its files in alphabetic order
+	 * empty folders area filtered from result
+	 */
+	public async getTitleSubFiles(titleObj: any) {
+		const subfolders = await fs.promises.readdir(titleObj.path)
+
+		const subfoldersPromises = subfolders.map(async (subName) => {
+			const path = titleObj.path + '/' + subName
+			const files = await fs.promises.readdir(path)
+			return files.filter(file => !this.prohibitedFormats.test(file) && this.allowedFormats.test(file))
+		})
+
+		try {
+			const fileList = await Promise.all(subfoldersPromises)
+			return fileList.filter(arr => arr.length >= 1)
+		} catch (error) {
+			console.log('getTitleSubFiles error', error)
+		}
+
+		// return list.filter(arr => arr.length >= 1)
 	}
 
 }
